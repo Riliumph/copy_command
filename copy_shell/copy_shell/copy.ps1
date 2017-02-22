a589ceb1bfe44ee19f94550437085754d380b640
Riliumph <riliumph@gmail.com>
2017-02-23 03:13:13 +0900

    [Update] power shell

---
+++ copy_shell/copy_shell/copy.ps1
@@ -0,0 +1,21 @@
+﻿#
+# Script.ps1
+#
+
+Param( [string]$copy_list, [string]$destination)
+Write-Debug "copy_list: $copy_list"
+Write-Debug "destination: $destination"
+
+Get-Date -Format '[yyyy/MM/dd HH:mm:ss]'
+$file_list = New-Object 'System.Collections.Generic.List[System.IO.FileInfo]'
+foreach( $line in (Get-Content -Path $copy_list -Encoding UTF8) -as [string[]] ){
+	$file = New-Object 'System.IO.FileInfo' ($line)
+	$file_list.Add($file)
+}
+
+#Get-Date -Format '[yyyy/MM/dd HH:mm:ss] Check destination'
+#if ( -not (Test-Path $destination) ){
+#    New-Item $destination -type directory
+#}
+
+
