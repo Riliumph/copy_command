9c9a88fcca6b02ce95400113672310d7a12d6ad8
Riliumph <riliumph@gmail.com>
2017-02-24 00:55:03 +0900

    [Update] powershell

--- copy_shell/copy_shell/copy.ps1
+++ copy_shell/copy_shell/copy.ps1
@@ -2,20 +2,40 @@
 # Script.ps1
 #
 
-Param( [string]$copy_list, [string]$destination)
+Param( 
+	[parameter(Mandatory=$true)][string]$copy_list,
+	[Parameter(Mandatory=$true)][string]$destination
+)
+
 Write-Debug "copy_list: $copy_list"
 Write-Debug "destination: $destination"
 
-Get-Date -Format '[yyyy/MM/dd HH:mm:ss]'
+Write-Host ("[{0}] Get file path from list..." -f (Get-Date -Format 'yyyy/MM/dd HH:mm:ss').ToString())
 $file_list = New-Object 'System.Collections.Generic.List[System.IO.FileInfo]'
+$miss_list = New-Object 'System.Collections.Generic.List[System.IO.FileInfo]'
 foreach( $line in (Get-Content -Path $copy_list -Encoding UTF8) -as [string[]] ){
 	$file = New-Object 'System.IO.FileInfo' ($line)
-	$file_list.Add($file)
+	if ( $file.Exists ){
+		$file_list.Add($file)
+	}else{
+		$miss_list.Add($file)
+	}
 }
 
-#Get-Date -Format '[yyyy/MM/dd HH:mm:ss] Check destination'
-#if ( -not (Test-Path $destination) ){
-#    New-Item $destination -type directory
-#}
+Write-Host ("[{0}] Missing file quantity: {1}" -f 
+	(Get-Date -Format 'yyyy/MM/dd HH:mm:ss').ToString(),
+	$miss_list.Count
+)
+$miss_list | Set-Content ./miss_list.log -Encoding UTF8
 
+Write-Host ("[{0}] Check destination path..." -f (Get-Date -Format 'yyyy/MM/dd HH:mm:ss').ToString())
+if ( -not (Test-Path $destination) ){
+	New-Item $destination -type directory | Out-Null
+}
 
+foreach( $file in $file_list ){
+	Write-Host ("Copying ... {0}" -f $file )
+	Copy-Item -LiteralPath $file $destination
+}
+
+Write-Host ("[{0}] All is done!!" -f (Get-Date -Format 'yyyy/MM/dd HH:mm:ss').ToString())
