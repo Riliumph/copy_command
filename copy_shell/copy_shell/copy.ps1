fcb2d1cfe4bee54fa031d3149dc6de3f83910919
Riliumph <riliumph@gmail.com>
2017-02-26 06:56:59 +0900

    [Update] <Powershell> 書式変更
    現在時刻を取得するコードを短縮化

--- copy_shell/copy_shell/copy.ps1
+++ copy_shell/copy_shell/copy.ps1
@@ -10,7 +10,7 @@ Param(
 Write-Debug "copy_list: $copy_list"
 Write-Debug "destination: $destination"
 
-Write-Host ("[{0}] Get file path from list..." -f (Get-Date -Format 'yyyy/MM/dd HH:mm:ss').ToString())
+Write-Host ("[{0}] Get file path from list..." -f [DateTime]::Now )
 $file_list = New-Object 'System.Collections.Generic.List[System.IO.FileInfo]'
 $miss_list = New-Object 'System.Collections.Generic.List[System.IO.FileInfo]'
 foreach( $line in (Get-Content -Path $copy_list -Encoding UTF8) -as [string[]] ){
@@ -22,13 +22,10 @@ foreach( $line in (Get-Content -Path $copy_list -Encoding UTF8) -as [string[]] )
 	}
 }
 
-Write-Host ("[{0}] Missing file quantity: {1}" -f 
-	(Get-Date -Format 'yyyy/MM/dd HH:mm:ss').ToString(),
-	$miss_list.Count
-)
+Write-Host ("[{0}] Missing file quantity: {1}" -f [DateTime]::Now, $miss_list.Count)
 $miss_list | Set-Content ./miss_list.log -Encoding UTF8
 
-Write-Host ("[{0}] Check destination path..." -f (Get-Date -Format 'yyyy/MM/dd HH:mm:ss').ToString())
+Write-Host ("[{0}] Check destination path..." -f [DateTime]::Now)
 if ( -not (Test-Path $destination) ){
 	New-Item $destination -type directory | Out-Null
 }
@@ -38,4 +35,4 @@ foreach( $file in $file_list ){
 	Copy-Item -LiteralPath $file $destination
 }
 
-Write-Host ("[{0}] All is done!!" -f (Get-Date -Format 'yyyy/MM/dd HH:mm:ss').ToString())
+Write-Host ("[{0}] All is done!!" -f [DateTime]::Now)
